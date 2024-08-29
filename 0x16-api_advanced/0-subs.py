ed899a687ef27cad3323d1a6c12447808be9f3e4
HackerSkull <hackerskull@HackerSkull.hackerskull>
2024-08-29 04:45:32 -0400

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
+
 """
-importing requests modeule
+importing requests module
 """
 
 from requests import get
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """
-    function that requires the Reddit API and returns the number of subscribers
-    for a given subreddit
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
     """
 
     if subreddit is None or not isinstance(subreddit, str):
-        return 0
-    
+        print("None")
+
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
     all_data = response.json()
 
     try:
-        return all_data.get('data').get('subscribers')
-    
+        raw1 = all_data.get('data').get('children')
+
+        for i in raw1:
+            print(i.get('data').get('title'))
+
     except:
-        return 0
+        print("None")
